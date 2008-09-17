b541ced377d95c74a5e4fecb9e3035ffb297b000
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2008-09-17 14:29:58 +0300

    New files.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+from fnmatch import fnmatch
+import os
+
+scripts = []
+for file in os.listdir('.'):
+    if fnmatch(file, '*.py') or fnmatch(file, '*.cgi'):
+        scripts.append(file)
+scripts.remove('setup.py')
+print scripts
+
+setup(name='syshwinfo',
+      version='2008.2',
+      description='Utilities for gathering system information and sending them to a central server.',
+      author='Janne Blomqvist',
+      author_email='Janne.Blomqvist@tkk.fi',
+      license = 'GPLv3',
+      url='http://www.fyslab.hut.fi/~/job/',
+      scripts = scripts
+     )
+
