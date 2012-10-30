33caf18efe37abf08bd7367621bec0a97f8fff28
Janne Blomqvist <Janne.Blomqvist@aalto.fi>
2012-10-30 15:13:37 +0200

    Tweak setup to include relevant files in tarball

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 
 from distutils.core import setup
-from glob import glob
-
-# Scripts whose names end in a-z or 1-9 (avoids emacs backup files)
-scripts = glob('scripts/*[a-z,1-9]')
 
 setup(name='syshwinfo',
       version='2.0',
@@ -12,7 +8,6 @@ setup(name='syshwinfo',
       author='Janne Blomqvist',
       author_email='Janne.Blomqvist@aalto.fi',
       license = 'MIT',
-      url='https://github.com/jabl/syshwinfo',
-      scripts = scripts
+      url='https://github.com/jabl/syshwinfo'
      )
 
